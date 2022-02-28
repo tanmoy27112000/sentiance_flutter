ad70437a44d8bebe90bc02220a3b87c5de931804
abhishekdixit <abhishek.dixit@gytworkz.com>
2022-02-28 16:36:42 +0530

    "New Branch for andorid SDK"

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:sentiance_flutter/models/mobile_health_data.dart';
 import 'package:sentiance_flutter/sentiance_flutter.dart';
 
 void main() {
@@ -15,155 +14,81 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var isLocationEnabled = false;
-  var isSentianceStoped = false;
-  var isHealthDataFetched = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _getSentianceData();
-  }
-
-  _getSentianceData() async {
-    var data = await SentianceFlutter.getSentianceData;
-    print("got data in app" + data);
-    if (data == "STARTED") {
-      setState(() {
-        isLocationEnabled = true;
-      });
-    } else {
-      setState(() {
-        isLocationEnabled = false;
-      });
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            _getPaymentMethodOption(context, "Enable Permissions"),
-          ],
-        ),
+        body: _getPaymentMethodOption(context),
       ),
     );
   }
 
-  String token =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjI5MjBmZjc0LTE1ZGItNGJiMy04ZWRjLTdhYjUzNjhkYTc3ZCIsImlhdCI6MTY0NTUwNzIxM30.o8RDqLsHR0nixlna1u53UV7EnA5B7M6SqhrwfzFKSuk";
+  String token = // we get the token from the app
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjRkNGM4NjhkLWJmMzMtNDYwMi05OGY2LTNkOTlkNjZjODRiMCIsImlhdCI6MTY0NTY3OTI4Nn0.S5wYYuKYGyXfkNAwFZz8KR_0Hn9TMrorWfGAK2TFDE0";
   String sentianceSecret =
-      "a2bc95ee20eacc70fbfb4bb15122356a0fcc11ebec0bfad76dfa6338caf74a7871c7ebe3fb6cb02633905ac61673bace5322f5352da9e24a67f1f51957ba2325";
-  String appId = "5faaa10ea972a10600000027";
-  String userLinkUrl = "https://devmobileapi.safetyconnect.io/user/link";
+      "a2bc95ee20eacc70fbfb4bb15122356a0fcc11ebec0bfad76dfa6338caf74a7871c7ebe3fb6cb02633905ac61673bace5322f5352da9e24a67f1f51957ba2325"; //constant
+  String appId = "5faaa10ea972a10600000027"; // constant
+  String userLinkUrl =
+      "https://devmobileapi.safetyconnect.io/user/link"; // constant
   String cashDetectionUrl = "";
   String mobileHealthUrl =
-      "https://devmobileapi.safetyconnect.io/me/mobilehealth";
+      "https://devmobileapi.safetyconnect.io/me/mobilehealth"; //constant
 
   // used for the payment option
   Widget _getPaymentMethodOption(
     BuildContext context,
-    String paymentName,
   ) {
-    return Column(
-      children: [
-        ListTile(
-          title: const Text(
-            "enable location",
-            style: TextStyle(
-              color: Colors.grey,
-              fontSize: 16.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          trailing: CupertinoSwitch(
-            trackColor: Colors.grey[300], // **INACTIVE STATE COLOR**
-            activeColor: Colors.black, // **ACTIVE STATE COLOR**
-            value: isLocationEnabled,
-            onChanged: (bool value) async {
-              if (isLocationEnabled == true) {
-                setState(() {
-                  isLocationEnabled = value;
-                });
-              } else {
-                setState(() {
-                  isLocationEnabled = true;
-                });
+    return Container(
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          ElevatedButton(
+              onPressed: () async {
                 await SentianceFlutter.initialiseSentiance(
-                    token,
-                    sentianceSecret,
-                    appId,
-                    userLinkUrl,
-                    cashDetectionUrl,
-                    mobileHealthUrl);
-              }
-            },
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 20, right: 20),
-          child: Divider(
-            color: Colors.blue[500],
-            height: 4,
-          ),
-        ),
-        ListTile(
-          title: const Text(
-            "Stop the SDK",
-            style: TextStyle(
-              color: Colors.grey,
-              fontSize: 16.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          trailing: CupertinoSwitch(
-              trackColor: Colors.grey[300], // **INACTIVE STATE COLOR**
-              activeColor: Colors.black, // **ACTIVE STATE COLOR**
-              value: isSentianceStoped,
-              onChanged: (bool value) async {
-                if (value) {
-                  await SentianceFlutter.stopSentianceSDK;
+                    token: token,
+                    sentianceSecret: sentianceSecret,
+                    appId: appId,
+                    userLinkUrl: userLinkUrl,
+                    crashDetectionUrl: cashDetectionUrl,
+                    mobileHealthUrl: mobileHealthUrl);
+              },
+              child: const Text(
+                "Initialise SDK",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              )),
+          ElevatedButton(
+              onPressed: () async {
+                var data = await SentianceFlutter.getMobileHealthData;
+                if (kDebugMode) {
+                  print(data.androidsdkVersion);
                 }
-                setState(() {
-                  isSentianceStoped = !isSentianceStoped;
-                });
-              }),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 20, right: 20),
-          child: Divider(
-            color: Colors.blue[500],
-            height: 4,
-          ),
-        ),
-        ListTile(
-          title: const Text(
-            "Get health Data",
-            style: TextStyle(
-              color: Colors.grey,
-              fontSize: 16.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          trailing: CupertinoSwitch(
-              trackColor: Colors.grey[300], // **INACTIVE STATE COLOR**
-              activeColor: Colors.black, // **ACTIVE STATE COLOR**
-              value: isHealthDataFetched,
-              onChanged: (bool value) async {
-                if (value) {
-                  var data = await SentianceFlutter.getMobileHealthData;
-                  print(data.brand);
-                }
-                setState(() {
-                  isHealthDataFetched = !isHealthDataFetched;
-                });
-              }),
-        ),
-      ],
+              },
+              child: const Text("Get Mobile Health Data",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ))),
+          ElevatedButton(
+              onPressed: () async {
+                await SentianceFlutter.stopSentianceSDK;
+              },
+              child: const Text(
+                "Stop SDK",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              )),
+        ],
+      ),
     );
   }
 }
