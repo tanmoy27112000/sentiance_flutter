c850db04f0db0e657c68523402fa9992aeb79391
abhishekdixit <abhishek.dixit@gytworkz.com>
2022-03-07 10:41:26 +0530

    "Remove the credentaisl"

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -85,20 +85,14 @@ class _MyAppState extends State<MyApp> {
                 await SentianceFlutter.initialiseSentiance(
                   {
                     "data": {
-                      "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU0ODc0YzkyLWM2MzgtNGZiZi04ZjcwLWY0YmE3NGQxMTg1ZSIsImlhdCI6MTY0MDg2MDg1MH0.ZqNp_NNvBPXvX306EHHRdvvahZG_Ps1BhMQ_BqgZw9g",
-                      "sentiance_secret" :"a2bc95ee20eacc70fbfb4bb15122356a0fcc11ebec0bfad76dfa6338caf74a7871c7ebe3fb6cb02633905ac61673bace5322f5352da9e24a67f1f51957ba2325",
+                      "token": "",
+                      "sentiance_secret" :"",
                   //: "3831007f47033a9c9a90ffa415da1465130e3cec6d974f17fe16e0c6229a00398173509b41c78b4e290323c4cfc415029ed1abbcca1dc70fc58a8ce0a874ef4b",
-                  "app_id": "5faaa10ea972a10600000027",
+                  "app_id": "",
                   "user_link_url":
-                  "https://mobileapi.safetyconnect.io/user/link",
+                  "",
                   "email": ""
-                      // "sentiance_secret":
-                      // "a2bc95ee20eacc70fbfb4bb15122356a0fcc11ebec0bfad76dfa6338caf74a7871c7ebe3fb6cb02633905ac61673bace5322f5352da9e24a67f1f51957ba2325",
-                      // "app_id": "5faaa10ea972a10600000027",
-                      // "user_link_url":
-                      // "https://devmobileapi.safetyconnect.io/user/link",
-                      //  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjliOWI5OTc4LWQxNjEtNDNmNy1hOTU0LWU4OTMyY2I2ZGUzZSIsImlhdCI6MTYzOTczNjEzNn0.vXgqIKHZ3DWv89cC2xSqStVpd1icb5Zdc1lbMsk3DE8",
-                      // "email": "tanmoykarmakar@gytworkz.com"
+
                     }
                   },
                 );
